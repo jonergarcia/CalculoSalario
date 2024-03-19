6797adc8d6f462c31f86d004d6562a0975b6d8bd
jonergarcia <jonergarcia>
2024-03-19 16:40:31 -0300

    subindo programa CalculoFuncionário sem funções

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AtividadeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculoFuncionario funcionario = new CalculoFuncionario();
+
+            Console.WriteLine("Digite o nome do Funcionário:");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Digite o Salário Bruto:");
+            double SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o imposto:");
+            int Imposto = int.Parse(Console.ReadLine());
+            double salarioLiquido = funcionario.CalcularSalarioLiquido(Imposto, SalarioBruto);
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Salário Líquido: {salarioLiquido}");
+
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine("Informe a porcentagem de aumento:");
+            int aumentoPorcentagem = int.Parse(Console.ReadLine());
+
+            double aumento = SalarioBruto * aumentoPorcentagem / 100;
+            SalarioBruto += aumento;
+            salarioLiquido = funcionario.CalcularSalarioLiquido(Imposto, SalarioBruto);
+            Console.WriteLine($"Novo Salário Líquido: {salarioLiquido}");
+        }
+    }
+}
---
+++ SalarioFuncionario.cs
@@ -0,0 +1,17 @@
+﻿namespace AtividadeSalario
+{
+    class CalculoFuncionario
+    {
+        public double CalcularSalarioLiquido(int imposto, double salario)
+        {
+            double desconto = salario * imposto / 100;
+            return salario - desconto;
+        }
+
+        public double AumentarSalario(double salario, double aumentoPorcentagem)
+        {
+            double aumento = salario * aumentoPorcentagem / 100;
+            return salario + aumento;
+        }
+    }
+}
